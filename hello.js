893085c38f8bd99e94edf128656e8485b27d77cb
geonsoon <qkrrjstns23@gmail.com>
2025-10-17 09:35:22 +0900

    test1_main_eat lunch입니다

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-console.log("goodmorning 입니다.")
-console.log("goodmorning 입니다.")
-console.log("goodmorning 입니다.")
-console.log("goodmorning 입니다.")
+console.log("main은 eat lunch 입니다.")
+console.log("main은 eat lunch 입니다.")
+console.log("main은 eat lunch 입니다.")
+console.log("main은 eat lunch 입니다.")
